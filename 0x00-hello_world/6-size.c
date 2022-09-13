935d0413374c759cf53a2b04a70b0a4d65a42e58
TheSHOGBAMU <shogbamuolamide@outlook.com>
2022-09-12 20:54:29 -0700

    modifies 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
-int main() {
+int main()
+{
     int integerType;
+    long int long_intType;
     float floatType;
     double doubleType;
     char charType;
 
     printf("Size of int: %ld bytes\n",sizeof(integerType));
+    printf("Size of a long int: %ld bytes\n", sizeof(long_intType));
     printf("Size of float: %ld bytes\n",sizeof(floatType));
     printf("Size of double: %ld bytes\n",sizeof(doubleType));
     printf("Size of char: %ld byte\n",sizeof(charType));

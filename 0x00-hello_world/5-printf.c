30bc66b4d4af28e014bab6df3583478adc4c3508
TheSHOGBAMU <shogbamuolamide@outlook.com>
2022-09-26 20:58:56 -0700

    modified 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,19 +1,10 @@
-include <stdio.h>
-
-
+#include <stdio.h>
 
 /**
-
- * main - print the data wrap in the string followed by a new line
-
+ * main - prints phrase
+ * Return: returns 0
  */
-
-int main(void)
-
-{
-
-    printf("with proper grammar, but the outcome is a piece of art,\n");
-
-    return (0);
-
+int main(void){
+	printf("with proper grammer, but the outcome is a piece of art, \n");
+	return (0);
 }

d642e947591090aa8f4b5df227a3dcaf6625ed51
TheSHOGBAMU <shogbamuolamide@outlook.com>
2022-09-26 20:52:02 -0700

    modified 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-/* Prints the string in the puts() function using the output library of Standard IO*/
+
+/**
+  *main - prints a string
+ * return: 0
+ */
 int main(void){
 puts("Programming is like building a multilingual puzzle\n");
 return (0);

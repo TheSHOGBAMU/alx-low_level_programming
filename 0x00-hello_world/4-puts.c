50ebfaf9aabe88b7100ecd9710a9306aec222dec
TheSHOGBAMU <shogbamuolamide@outlook.com>
2022-09-26 21:21:10 -0700

    modified 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - prints a string
- * Return - returns 0
+ * main - prints strings
+ * Return: returns 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
